0c8ccbf5d5207be8b55fea23aa62b1cef4722f5a
Danking7 <daniel.j.king2004@gmail.com>
2024-02-29 12:16:24 +0000

    updated

---
+++ projectworldenew/implementation.cpp
@@ -0,0 +1,8 @@
+//
+//  implementation.cpp
+//  projectworldenew
+//
+//  Created by Dan King on 29/02/2024.
+//
+
+#include "implementation.hpp"
---
+++ projectworldenew/implementation.hpp
@@ -0,0 +1,13 @@
+//
+//  implementation.hpp
+//  projectworldenew
+//
+//  Created by Dan King on 29/02/2024.
+//
+
+#ifndef implementation_hpp
+#define implementation_hpp
+
+#include <stdio.h>
+
+#endif /* implementation_hpp */
